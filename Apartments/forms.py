02aab8cffe2e19cb89b92077b34cf0ab788fb0f9
Vanquay <vanquay16@gmail.com>
2020-11-24 23:32:47 -0500

    Version 1

--- Apartments/forms.py
+++ Apartments/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.forms import ModelForm
 from .models import TenantUser,WorkOrders
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+
 
 
 
